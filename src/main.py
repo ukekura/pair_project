599f36ec226b1c1fe7469ef413e40d5baf709867
ukekura <ukekura@sabira.jp>
2025-06-19 11:01:05 +0900

    少し修正

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import json
+import os
 
-if __name__ == "__main__":
-    
+def main():
     with open("input/invoices.json", "r", encoding="utf-8") as f:
         invoices = json.load(f)
     with open("input/plays.json", "r", encoding="utf-8") as f:
@@ -15,8 +15,7 @@ if __name__ == "__main__":
     invoice_content += "\n\nplays.jsonの内容:\n"
     invoice_content += json.dumps(plays, ensure_ascii=False, indent=2)
 
-     # 出力ディレクトリの作成（存在しない場合）
-    import os
+    # 出力ディレクトリの作成（存在しない場合）
     os.makedirs("output", exist_ok=True)
     
     # ファイルに出力
@@ -24,3 +23,6 @@ if __name__ == "__main__":
         f.write(invoice_content)
     
     print("請求書が正常に出力されました。")
+
+if __name__ == "__main__":
+    main()

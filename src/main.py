a768d7158245de80a5d724bc888d3a4e394a0dc9
gibo <211453139+tomoya-gibo@users.noreply.github.com>
2025-06-24 11:32:37 +0900

    既存のコードをコピペした。

--- src/main.py
+++ src/main.py
@@ -41,6 +41,39 @@ def main():
     invoice_content += "合計金額：$" + str(total_price) +  "\n"
     invoice_content += "獲得ポイント：" + str(total_point) + "pt"
 
+    
+    
+    invoice_content = "請求書\n"
+
+    invoice_content += invoices[0]["customer"] + "\n"
+
+    total_price = 0
+    total_point = 0
+
+    for performance in invoices[0]["performances"]:
+
+        if plays[performance["playID"]].get("type") == "tragedy":
+            price = 40000
+            if performance["audience"] > 30:
+                price += (performance["audience"] - 30) * 1000 
+        
+        if plays[performance["playID"]].get("type") == "comedy":
+            price = 30000 + performance["audience"] * 300
+            if performance["audience"] > 20:
+                price += (performance["audience"] - 20) * 500 + 10000             
+            total_point += performance["audience"] // 5
+
+        if performance["audience"]  > 30:
+            total_point += (performance["audience"] - 30)
+
+        invoice_content += "・" + plays[performance["playID"]]["name"] + "（観客数：" + str(performance["audience"]) + "人、金額：$"+ str(price) + "）\n"
+
+        total_price += price
+
+    invoice_content += "合計金額：$" + str(total_price) +  "\n"
+    invoice_content += "獲得ポイント：" + str(total_point) + "pt"
+
+
     # 出力ディレクトリの作成（存在しない場合）
     os.makedirs("output", exist_ok=True)
     

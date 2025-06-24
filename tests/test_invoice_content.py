3ebc351e39746139d31220de52e8ce304237d97e
ukekura <ukekura@sabira.jp>
2025-06-24 18:02:15 +0900

    mockerを使うように

--- tests/test_invoice_content.py
+++ tests/test_invoice_content.py
@@ -2,9 +2,13 @@ import json
 import os
 from src.main import main
 
+def excute_main(mocker):
+   mocker.patch("src.main.load_json", load_json_for_test)
+   main()
+
 # jsonファイルの読み込み
-def load_json():
-   with open("input/invoices.json", "r", encoding="utf-8") as f:
+def load_json_for_test():
+   with open("tests/mock.json", "r", encoding="utf-8") as f:
         invoices = json.load(f)
    with open("input/plays.json", "r", encoding="utf-8") as f:
         plays = json.load(f)
@@ -23,46 +27,46 @@ def get_all_text_invoice():
    return result
 
 # 「請求書」というタイトルがあるか
-def test_invoice_has_title():
-   main()
+def test_invoice_has_title(mocker):
+   excute_main(mocker)
    invoice = get_lines_text_invoice()
    assert invoice[0].strip() == "請求書"
 
 # 会社名があるか
-def test_invoice_has_customer_name():
-   main()
+def test_invoice_has_customer_name(mocker):
+   excute_main(mocker)
    invoice = get_lines_text_invoice()
    assert invoice[1].strip() == "BigCo"
 
 # Hamlet
-def test_hamlet():
-   main()
+def test_hamlet(mocker):
+   excute_main(mocker)
    invoice = get_all_text_invoice()
    assert '・Hamlet（観客数：55人、金額：$65000）' in invoice
 
 # As You Like It
-def test_as_like():
-   main()
+def test_as_like(mocker):
+   excute_main(mocker)
    invoice = get_all_text_invoice()
    assert '・As You Like It（観客数：35人、金額：$58000）' in invoice
 
 # Othello
-def test_othello():
-   main()
+def test_othello(mocker):
+   excute_main(mocker)
    invoice = get_all_text_invoice()
    assert '・Othello（観客数：40人、金額：$50000）' in invoice
 
 # 合計金額
-def test_total_price():
-   main()
+def test_total_price(mocker):
+   excute_main(mocker)
    invoice = get_lines_text_invoice()
    total_price_line = invoice[-2].strip()
    total_price = int(total_price_line.split("合計金額：$")[1])
    assert total_price == 173000
 
 # 獲得ポイント
-def test_total_price():
-   main()
+def test_total_price(mocker):
+   excute_main(mocker)
    invoice = get_lines_text_invoice()
    total_price_line = invoice[-1].strip()
    total_price = int(total_price_line.split("獲得ポイント：")[1].split("pt")[0])

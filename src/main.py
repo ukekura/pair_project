6e8f53f7a2aaa292c1bf243a05eb5ec7e8dcaed6
ukekura <ukekura@sabira.jp>
2025-07-01 10:52:59 +0900

    16-2_パラメータによる問い合わせの置き換え_問い合わせ以外の抽出

--- src/main.py
+++ src/main.py
@@ -20,7 +20,40 @@ def main():
         return invoices
     
     def new_new_calc_price_point(invoices, plays, total_price, total_point):
-        return
+        for performance in invoices["performances"]:
+
+            if plays[performance["playID"]].get("type") == "tragedy":
+                price = 40000
+                if performance["audience"] > 30:
+                    price += (performance["audience"] - 30) * 1000 
+
+            if plays[performance["playID"]].get("type") == "comedy":
+                price = 30000 + performance["audience"] * 300
+                if performance["audience"] > 20:
+                    price += (performance["audience"] - 20) * 500 + 10000             
+                total_point += performance["audience"] // 5
+
+            if performance["audience"]  > 30:
+                total_point += (performance["audience"] - 30)
+
+            total_price += price
+
+
+        for performance in invoices["performances"]:
+
+            if plays[performance["playID"]].get("type") == "tragedy":
+                price = 40000
+                if performance["audience"] > 30:
+                    price += (performance["audience"] - 30) * 1000 
+
+            if plays[performance["playID"]].get("type") == "comedy":
+                price = 30000 + performance["audience"] * 300
+                if performance["audience"] > 20:
+                    price += (performance["audience"] - 20) * 500 + 10000
+
+            invocie_contenta = invocie_contenta + "・" + plays[performance["playID"]]["name"] + "（観客数：" + str(performance["audience"]) + "人、金額：$"+ str(price) + "）\n"
+        
+        return invocie_contenta, total_price, total_point
     
     def new_calc_price_point(invoices, plays, total_price, total_point):
         invocie_contenta = "請求書\n"

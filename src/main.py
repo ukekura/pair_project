d643fe2d828c6647596738a6aad95a75e0325ef6
ukekura <ukekura@sabira.jp>
2025-07-01 10:34:19 +0900

    再々やり直し

--- src/main.py
+++ src/main.py
@@ -8,48 +8,64 @@ def load_json():
         plays = json.load(f)
    return invoices, plays
 
-def main():
-    invoices, plays = load_json()
-
-    invoice_content = "請求書\n"
-
-    invoice_content += invoices[0]["customer"] + "\n"
-
-    total_price = 0
-    total_point = 0
-
-
-    for performance in invoices[0]["performances"]:
-
-        if plays[performance["playID"]].get("type") == "tragedy":
-            price = 40000
-            if performance["audience"] > 30:
-                price += (performance["audience"] - 30) * 1000 
-        
-        if plays[performance["playID"]].get("type") == "comedy":
-            price = 30000 + performance["audience"] * 300
-            if performance["audience"] > 20:
-                price += (performance["audience"] - 20) * 500 + 10000             
-            total_point += performance["audience"] // 5
-
-        if performance["audience"]  > 30:
-            total_point += (performance["audience"] - 30)
-
-        invoice_content += "・" + plays[performance["playID"]]["name"] + "（観客数：" + str(performance["audience"]) + "人、金額：$"+ str(price) + "）\n"
-
-        total_price += price
-
-    invoice_content += "合計金額：$" + str(total_price) +  "\n"
-    invoice_content += "獲得ポイント：" + str(total_point) + "pt"
-
-    # 出力ディレクトリの作成（存在しない場合）
+def output_text(invoice_content):
     os.makedirs("output", exist_ok=True)
-    
-    # ファイルに出力
     with open("output/invoice.txt", "w", encoding="utf-8") as f:
         f.write(invoice_content)
-    
     print("請求書が正常に出力されました。")
+
+def main():
+    def format_invoice_data(invoices):
+        invoices = invoices[0]
+        return invoices
+    
+    def calc_price_point(invoices, plays):
+        total_price = 0
+        total_point = 0
+
+        for performance in invoices["performances"]:
+
+            if plays[performance["playID"]].get("type") == "tragedy":
+                price = 40000
+                if performance["audience"] > 30:
+                    price += (performance["audience"] - 30) * 1000 
+
+            if plays[performance["playID"]].get("type") == "comedy":
+                price = 30000 + performance["audience"] * 300
+                if performance["audience"] > 20:
+                    price += (performance["audience"] - 20) * 500 + 10000             
+                total_point += performance["audience"] // 5
+
+            if performance["audience"]  > 30:
+                total_point += (performance["audience"] - 30)
+
+            total_price += price
+
+        invocie_contenta = "請求書\n"
+        invocie_contenta += invoices["customer"] + "\n"
+
+        for performance in invoices["performances"]:
+
+            if plays[performance["playID"]].get("type") == "tragedy":
+                price = 40000
+                if performance["audience"] > 30:
+                    price += (performance["audience"] - 30) * 1000 
+
+            if plays[performance["playID"]].get("type") == "comedy":
+                price = 30000 + performance["audience"] * 300
+                if performance["audience"] > 20:
+                    price += (performance["audience"] - 20) * 500 + 10000
+
+            invocie_contenta = invocie_contenta + "・" + plays[performance["playID"]]["name"] + "（観客数：" + str(performance["audience"]) + "人、金額：$"+ str(price) + "）\n"
+        
+        return invocie_contenta, total_price, total_point
+
+    invoices, plays = load_json()
+    invoices = format_invoice_data(invoices)
+    invoice_content, total_price, total_point = calc_price_point(invoices, plays)
+    invoice_content += "合計金額：$" + str(total_price) +  "\n"
+    invoice_content += "獲得ポイント：" + str(total_point) + "pt"
+    output_text(invoice_content)
 
 if __name__ == "__main__":
     main()

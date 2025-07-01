2fba5f6b2cf719435589095ea45273791f2bc401
ukekura <ukekura@sabira.jp>
2025-07-01 11:20:13 +0900

    20-2_関数のインライン化？

--- src/main.py
+++ src/main.py
@@ -53,14 +53,6 @@ def main():
             invocie_contenta = invocie_contenta + "・" + plays[performance["playID"]]["name"] + "（観客数：" + str(performance["audience"]) + "人、金額：$"+ str(price) + "）\n"
         
         return invocie_contenta, total_price, total_point
-    
-    def new_calc_price_point(invoices, plays, total_price, total_point):
-        invocie_contenta = "請求書\n"
-        invocie_contenta += invoices["customer"] + "\n"
-
-        invocie_contenta, total_price, total_point = new_new_calc_price_point(invoices, plays, total_price, total_point, invocie_contenta)
-
-        return invocie_contenta, total_price, total_point
 
     invoices, plays = load_json()
     invoices = format_invoice_data(invoices)

8711535793fac1e9db25b1037bb3462723671c74
ukekura <ukekura@sabira.jp>
2025-07-01 14:58:50 +0900

    47-2_フェーズの分離_中間データ構造の導入

--- src/main.py
+++ src/main.py
@@ -60,13 +60,16 @@ def main():
         return total_price, total_point
     
     def create_trade_content(invocie_contenta):
-        def format_faze(invocie_contenta):
+
+        def format_faze(mid_data, invocie_contenta):
             for performance in INVOICES["performances"]:
                 price = check_type_calc_price(performance)
                 invocie_contenta = invocie_contenta + "・" + PLAYS[performance["playID"]]["name"] + "（観客数：" + str(performance["audience"]) + "人、金額：$"+ str(price) + "）\n"
             return invocie_contenta
         
-        invocie_contenta = format_faze(invocie_contenta)
+        mid_data = []
+        invocie_contenta = format_faze(mid_data, invocie_contenta)
+        
         return invocie_contenta
     
     def finish_invoice_content(invoice_content, total_price, total_point):

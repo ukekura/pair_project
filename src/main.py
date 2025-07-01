7c1c9df586fa9e0d71d8dd69280f5a057d386a61
gibo <211453139+tomoya-gibo@users.noreply.github.com>
2025-07-01 11:29:44 +0900

    [ループの分離]計算部分と整形部分を分けることができなかったので、ループの分離を破棄した。

--- src/main.py
+++ src/main.py
@@ -39,26 +39,6 @@ def main():
 
         total_price += price
 
-    for performance in invoices[0]["performances"]:
-
-        if plays[performance["playID"]].get("type") == "tragedy":
-            price = 40000
-            if performance["audience"] > 30:
-                price += (performance["audience"] - 30) * 1000 
-        
-        if plays[performance["playID"]].get("type") == "comedy":
-            price = 30000 + performance["audience"] * 300
-            if performance["audience"] > 20:
-                price += (performance["audience"] - 20) * 500 + 10000             
-            total_point += performance["audience"] // 5
-
-        if performance["audience"]  > 30:
-            total_point += (performance["audience"] - 30)
-
-        invoice_content += "・" + plays[performance["playID"]]["name"] + "（観客数：" + str(performance["audience"]) + "人、金額：$"+ str(price) + "）\n"
-
-        total_price += price
-
     invoice_content += "合計金額：$" + str(total_price) +  "\n"
     invoice_content += "獲得ポイント：" + str(total_point) + "pt"
 

c91cd76a58cebe61cb18f449a5d7978be89196f1
ukekura <ukekura@sabira.jp>
2025-07-02 11:33:31 +0900

    57-2_ステートメントの呼び出側への移動_移動したくないものの抽出

--- src/main.py
+++ src/main.py
@@ -54,6 +54,27 @@ def main():
         
         mid_data = add_price_to()
 
+        def new_calc_total_price_point():
+            def amoutn_price(mid_data):
+                total_price = 0
+                for performance in mid_data:
+                    price = check_type_calc_price(performance)
+                    total_price += price
+                return total_price
+
+            total_price = amoutn_price(mid_data)
+
+            total_point = 0
+            for performance in INVOICES["performances"]:
+                if PLAYS[performance["playID"]].get("type") == "comedy":
+                    total_point += performance["audience"] // 5
+                if performance["audience"]  > 30:
+                    total_point += (performance["audience"] - 30)
+                
+            return total_price, total_point
+        
+        return new_calc_total_price_point()
+
         def amoutn_price(mid_data):
             total_price = 0
             for performance in mid_data:

8a9ea8f48c58eeb5d0722169afc4146cd2594751
ukekura <ukekura@sabira.jp>
2025-07-02 13:58:39 +0900

    65-3_関数宣言の変更_関数名の変更

--- src/main.py
+++ src/main.py
@@ -80,7 +80,7 @@ def main():
 
     invoice_content = initialize_invoice_content()
 
-    def new_create_trade_content(invocie_contenta, mid_data):
+    def create_trade_content(invocie_contenta, mid_data):
         def format_faze(mid_data, invocie_contenta):
             for performance in mid_data:
                 invocie_contenta = invocie_contenta + "・" + PLAYS[performance["playID"]]["name"] + "（観客数：" + str(performance["audience"]) + "人、金額：$"+ str(performance["price"]) + "）\n"
@@ -88,7 +88,7 @@ def main():
         mid_data = add_price_key_to(INVOICES)
         invocie_contenta = format_faze(mid_data, invocie_contenta)
         return invocie_contenta
-    invoice_content = new_create_trade_content(invoice_content, mid_data)
+    invoice_content = create_trade_content(invoice_content, mid_data)
 
     invoice_content = finish_invoice_content(invoice_content, total_price, total_point)
 

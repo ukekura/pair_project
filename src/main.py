47527885426aaf161585b41954778135a0597e1b
ukekura <ukekura@sabira.jp>
2025-07-02 10:07:03 +0900

    50-4_フェーズの分離_前半フェーズの抽出

--- src/main.py
+++ src/main.py
@@ -68,6 +68,10 @@ def main():
 
 
         def add_price_to():
+            mid_data = INVOICES["performances"]
+            for performance in mid_data:
+                price = check_type_calc_price(performance)
+                performance["price"] = price
             return
         
         mid_data = INVOICES["performances"]

1a25a6236b8c156ff2d5e122036e72332a5705d1
ukekura <ukekura@sabira.jp>
2025-07-04 15:55:16 +0900

    new_new_mid_dataをnew_mid_dataに置き換え

--- src/main.py
+++ src/main.py
@@ -96,7 +96,7 @@ def main():
         performance["point"] = 0
         performance["point"] = check_type_calc_point(performance)
 
-    invoice_content = create_invoice_content_from(new_new_mid_data, total_price, total_point)
+    invoice_content = create_invoice_content_from(new_mid_data, total_price, total_point)
     
     output_text(invoice_content)
 

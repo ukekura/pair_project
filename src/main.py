346c0583993f174722d2b460750073d2c32caff8
gibo <211453139+tomoya-gibo@users.noreply.github.com>
2025-07-10 14:11:57 +0900

    [関数の移動]関数をコピーして一時的な名前に変更した。

--- src/main.py
+++ src/main.py
@@ -71,6 +71,19 @@ class Invoice:
         return self.performances
     def get_customer(self):
         return self.customer
+    
+    def class_format_invoice_content():
+        invoice_content = "請求書\n"
+        invoice_content += invoice.get_customer() + "\n"
+        
+        for performance in invoice.get_performances().get_performances():
+            invoice_content += "・" + performance.get_name() + "（観客数：" + str(performance.get_audience()) + "人、金額：$"+ str(performance.calc_price()) + "）\n"
+
+        invoice_content += "合計金額：$" + str(invoice.get_performances().calc_total_price()) +  "\n"
+        invoice_content += "獲得ポイント：" + str(invoice.get_performances().calc_total_point()) + "pt"
+
+        return invoice_content
+
 
 def load_json():
    with open("input/invoices.json", "r", encoding="utf-8") as f:

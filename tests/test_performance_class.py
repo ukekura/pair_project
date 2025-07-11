550039501a5a5e66c2c0bf61031f5bae04c84c89
gibo <211453139+tomoya-gibo@users.noreply.github.com>
2025-07-11 11:30:32 +0900

    calc_priceに対して最初のテストを実行した。

--- tests/test_performance_class.py
+++ tests/test_performance_class.py
@@ -1,4 +1,10 @@
 from src.main import Performance
 
 def test_calc_price():
-  pass
+  performance = Performance({
+        "name": "Hamlet",
+        "audience": 29,
+        "type": "tragedy"
+      })
+  price = performance.calc_price()
+  assert price == 40000

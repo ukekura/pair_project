fe11e9f5c0f08b8477ceb33a11aa43322acbc33d
gibo <211453139+tomoya-gibo@users.noreply.github.com>
2025-07-11 13:27:13 +0900

    tragedyの場合のテストを実装した。

--- tests/test_performance_class.py
+++ tests/test_performance_class.py
@@ -1,10 +1,38 @@
 from src.main import Performance
 
-def test_calc_price():
-  performance = Performance({
-        "name": "Hamlet",
-        "audience": 29,
-        "type": "tragedy"
-      })
-  price = performance.calc_price()
-  assert price == 40000
+class TestCalcPrice:
+  def test_tragedy_thirty_or_under(self):
+    performance = Performance({
+          "name": "Hamlet",
+          "audience": 30,
+          "type": "tragedy"
+        })
+    price = performance.calc_price()
+    assert price == 40000
+
+    performance = Performance({
+      "name": "Hamlet",
+      "audience": 23,
+      "type": "tragedy"
+    })
+    price = performance.calc_price()
+    assert price == 40000
+
+  def test_tragedy_over_thirty(self):
+    performance = Performance({
+          "name": "Hamlet",
+          "audience": 31,
+          "type": "tragedy"
+        })
+    price = performance.calc_price()
+    assert price == 41000
+
+    performance = Performance({
+          "name": "Hamlet",
+          "audience": 40,
+          "type": "tragedy"
+        })
+    price = performance.calc_price()
+    assert price == 50000
+
+

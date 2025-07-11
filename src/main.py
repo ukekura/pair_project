ec9a71e9f1dda6e830ff841427eb5d0bfc9bd625
ukekura <ukekura@sabira.jp>
2025-07-11 15:02:05 +0900

    ユニットテストをするために関数の定義をトップレベルに移動

--- src/main.py
+++ src/main.py
@@ -15,108 +15,96 @@ def output_text(invoice_content):
         f.write(invoice_content)
     print("請求書が正常に出力されました。")
 
-def main():
-    def preperate_invoice_data(invoices, plays):
-        def format_invoices(invoices):
-            invoices = invoices[0]
-            return invoices
-        
-        def deep_copy(arg):
-            result = copy.deepcopy(arg)
-            return result
-        
-        def integrate(invoice_data, plays):
-            for performance in invoice_data["performances"]:
-                performance["type"] = plays[performance["playID"]].get("type")
-                performance["name"] = plays[performance["playID"]]["name"]
-            return invoice_data
-        
-        invoices = format_invoices(invoices)
-        invoice_data = deep_copy(invoices)
-        integrated_invoice_data = integrate(invoice_data, plays)
-
-        return integrated_invoice_data
+def preperate_invoice_data(invoices, plays):
+    def format_invoices(invoices):
+        invoices = invoices[0]
+        return invoices
     
-    def create_invoice_content(invoice_data):
-        def calc_invoice_data(invoice_data):
-            def calc_price(invoice_data):
-                def clac_tragedy_price(performance):
-                    price = 40000
-                    if performance["audience"] > 30:
-                        price += (performance["audience"] - 30) * 1000 
-                    return price
+    def deep_copy(arg):
+        result = copy.deepcopy(arg)
+        return result
+    
+    def integrate(invoice_data, plays):
+        for performance in invoice_data["performances"]:
+            performance["type"] = plays[performance["playID"]].get("type")
+            performance["name"] = plays[performance["playID"]]["name"]
+        return invoice_data
+    
+    invoices = format_invoices(invoices)
+    invoice_data = deep_copy(invoices)
+    integrated_invoice_data = integrate(invoice_data, plays)
+    return integrated_invoice_data
 
-                def clac_comedy_price(performance):
-                    price = 30000 + performance["audience"] * 300
-                    if performance["audience"] > 20:
-                        price += (performance["audience"] - 20) * 500 + 10000
-                    return price
-
-                for performance in invoice_data["performances"]:
-                    if performance["type"] == "tragedy":
-                        price = clac_tragedy_price(performance)
-                    if performance["type"] == "comedy":
-                        price = clac_comedy_price(performance)
-                    performance["price"] = price
-
-                return invoice_data
-            
-            def calc_point(invoice_data):
-                for performance in invoice_data["performances"]:
-                    performance["point"] = 0
-                    if performance["type"] == "comedy":
-                        point = performance["audience"] // 5
-                        performance["point"] += point
-                    if performance["audience"]  > 30:
-                        point = (performance["audience"] - 30)
-                        performance["point"] += point
-
-                return invoice_data
-            
-            def calc_total_price_point(invoice_data):
-                def calc_total_price(invoice_data):
-                    total_price = 0
-                    for performance in invoice_data["performances"]:
-                        total_price += performance["price"]
-                    invoice_data["total_price"] = total_price
-                    return invoice_data
-                    
-                def calc_total_point(invoice_data):
-                    total_point = 0
-                    for performance in invoice_data["performances"]:
-                        total_point += performance["point"]
-                    invoice_data["total_point"] = total_point
-                    return invoice_data
-                
-                invoice_data = calc_total_price(invoice_data)
-                invoice_data = calc_total_point(invoice_data)
-                
-                return invoice_data
-            
-            invoice_data = calc_price(invoice_data)
-            invoice_data = calc_point(invoice_data)
-            invoice_data = calc_total_price_point(invoice_data)
-
-            return invoice_data
-
-        def format_invoice_content(invoice_data):
-            invoice_content = "請求書\n"
-            invoice_content += invoice_data["customer"] + "\n"
+def calc_invoice_data(invoice_data):
+    def calc_price(invoice_data):
+        def clac_tragedy_price(performance):
+            price = 40000
+            if performance["audience"] > 30:
+                price += (performance["audience"] - 30) * 1000 
+            return price
+        def clac_comedy_price(performance):
+            price = 30000 + performance["audience"] * 300
+            if performance["audience"] > 20:
+                price += (performance["audience"] - 20) * 500 + 10000
+            return price
+        for performance in invoice_data["performances"]:
+            if performance["type"] == "tragedy":
+                price = clac_tragedy_price(performance)
+            if performance["type"] == "comedy":
+                price = clac_comedy_price(performance)
+            performance["price"] = price
+        return invoice_data
+    
+    def calc_point(invoice_data):
+        for performance in invoice_data["performances"]:
+            performance["point"] = 0
+            if performance["type"] == "comedy":
+                point = performance["audience"] // 5
+                performance["point"] += point
+            if performance["audience"]  > 30:
+                point = (performance["audience"] - 30)
+                performance["point"] += point
+        return invoice_data
+    
+    def calc_total_price_point(invoice_data):
+        def calc_total_price(invoice_data):
+            total_price = 0
             for performance in invoice_data["performances"]:
-                invoice_content = invoice_content + "・" + performance["name"] + "（観客数：" + str(performance["audience"]) + "人、金額：$"+ str(performance["price"]) + "）\n"
-            invoice_content += "合計金額：$" + str(invoice_data["total_price"]) +  "\n"
-            invoice_content += "獲得ポイント：" + str(invoice_data["total_point"]) + "pt"
-
-            return invoice_content
+                total_price += performance["price"]
+            invoice_data["total_price"] = total_price
+            return invoice_data
+            
+        def calc_total_point(invoice_data):
+            total_point = 0
+            for performance in invoice_data["performances"]:
+                total_point += performance["point"]
+            invoice_data["total_point"] = total_point
+            return invoice_data
         
-        invoice_material = calc_invoice_data(invoice_data)
-        invoice_content = format_invoice_content(invoice_material)
+        invoice_data = calc_total_price(invoice_data)
+        invoice_data = calc_total_point(invoice_data)
+        
+        return invoice_data
+    
+    invoice_data = calc_price(invoice_data)
+    invoice_data = calc_point(invoice_data)
+    invoice_data = calc_total_price_point(invoice_data)
+    return invoice_data
 
-        return invoice_content
+def format_invoice_content(invoice_data):
+    invoice_content = "請求書\n"
+    invoice_content += invoice_data["customer"] + "\n"
+    for performance in invoice_data["performances"]:
+        invoice_content = invoice_content + "・" + performance["name"] + "（観客数：" + str(performance["audience"]) + "人、金額：$"+ str(performance["price"]) + "）\n"
+    invoice_content += "合計金額：$" + str(invoice_data["total_price"]) +  "\n"
+    invoice_content += "獲得ポイント：" + str(invoice_data["total_point"]) + "pt"
+    return invoice_content
 
+def main():
     invoices, plays = load_json()
     invoice_data = preperate_invoice_data(invoices, plays)
-    invoice_content = create_invoice_content(invoice_data)
+    invoice_material = calc_invoice_data(invoice_data)
+    invoice_content = format_invoice_content(invoice_material)
     output_text(invoice_content)
 
 if __name__ == "__main__":

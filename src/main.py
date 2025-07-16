1e89a09b0c8564e3a68e2368f372c2ae30c916c0
ukekura <ukekura@sabira.jp>
2025-07-16 15:30:39 +0900

    もともとセッターだったものを値取得のために呼び出し

--- src/main.py
+++ src/main.py
@@ -150,7 +150,7 @@ def format_invoice_content(invoice_data, performances):
     for performance in performances.get_performances():
         invoice_content = invoice_content + "・" + performance.get_name() + "（観客数：" + str(performance.get_audience()) + "人、金額：$"+ str(performance.price()) + "）\n"
     invoice_content += "合計金額：$" + str(performances.total_price()) +  "\n"
-    invoice_content += "獲得ポイント：" + str(performances.total_point()) + "pt"
+    invoice_content += "獲得ポイント：" + str(performances.set_total_point()) + "pt"
     return invoice_content
 
 def format_to_html(invoice_data, performances):
@@ -161,7 +161,7 @@ def format_to_html(invoice_data, performances):
         invoice_content = invoice_content + "<li>" + performance.get_name() + "（観客数：" + str(performance.get_audience()) + "人、金額：$"+ str(performance.price()) + "）</li>"
     invoice_content += "</ul>"
     invoice_content += "<p>" + "合計金額：$" + str(performances.total_price()) +  "</p>"
-    invoice_content += "<p>" + "獲得ポイント：" + str(performances.total_point()) + "pt</p>"
+    invoice_content += "<p>" + "獲得ポイント：" + str(performances.set_total_point()) + "pt</p>"
     return invoice_content
 
 def main():

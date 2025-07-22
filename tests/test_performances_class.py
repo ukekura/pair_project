289cb1b1c08f3edd56c300f9c18d86c90229b79b
gibo <211453139+tomoya-gibo@users.noreply.github.com>
2025-07-22 10:59:20 +0900

    calc_total_priceのテストを実装した。

--- tests/test_performances_class.py
+++ tests/test_performances_class.py
@@ -40,3 +40,45 @@ class TestCalcTotalPrice:
     })
     total_price = performances.calc_total_price()
     assert total_price == 99000
+
+
+class TestCalcTotalPoint:
+  def test_calc_total_point(self):
+    performances = Performances([
+      {
+        "playID" : "hamlet",
+        "audience" : 31
+      },
+      {
+        "playID" : "as-like",
+        "audience" : 35
+      },
+      {
+        "playID" : "othello",
+        "audience" : 31
+      }
+    ],
+    {
+      "hamlet" : {"name": "Hamlet", "type": "tragedy"},
+      "as-like" : {"name": "As You Like It", "type": "comedy"},
+      "othello" : {"name": "Othello", "type": "tragedy"}
+    })
+    total_point = performances.calc_total_point()
+    assert total_point == 14
+
+    performances = Performances([
+      {
+        "playID" : "hamlet",
+        "audience" : 31
+      },
+      {
+        "playID" : "as-like",
+        "audience" : 35
+      }
+    ],
+    {
+      "hamlet" : {"name": "Hamlet", "type": "tragedy"},
+      "as-like" : {"name": "As You Like It", "type": "comedy"},
+    })
+    total_point = performances.calc_total_point()
+    assert total_point == 13

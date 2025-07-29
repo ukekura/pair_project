b5dddb262716bbdc6505d784d5233d4c931cb99d
ukekura <ukekura@sabira.jp>
2025-07-29 15:11:35 +0900

    pointも同様

--- src/main.py
+++ src/main.py
@@ -23,11 +23,7 @@ class Performance:
         return price
     
     def point(self):
-        point = 0
-        if self.__get_type() == "comedy":
-            point += self.__audience // 5
-        if self.audience()  > 30:
-            point += (self.__audience - 30)
+        point = SomethingToCalculate().calc_point(self.__type, self.__audience)
         return point
     
 class SomethingToCalculate:
